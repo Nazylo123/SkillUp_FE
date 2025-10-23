dc1470476840665d1e5ec54acb6da06a882dafcf
vietahh <vietahh02@gmail.com>
2025-10-23 20:28:43 +0700

    quizz for lecturer

---
+++ src/app/aproject/lecturer/course-management/final-quiz/final-quiz.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+
+@Component({
+    selector: 'app-lecturer-final-quiz',
+    imports: [RouterOutlet],
+    templateUrl: './final-quiz.component.html',
+    styleUrls: ['./final-quiz.component.scss'],
+})
+export class LecturerFinalQuiz {}
