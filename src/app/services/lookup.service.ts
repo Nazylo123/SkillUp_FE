150f0c66bf01ff3a34edaa5c340a3deb3fa73d8f
vietahh <vietahh02@gmail.com>
2025-12-10 01:20:16 +0700

    fix fe, course type, user level

--- src/app/services/lookup.service.ts
+++ src/app/services/lookup.service.ts
@@ -17,8 +17,32 @@ export class ApiLookupServices {
     return this.http.get<CourseType[]>(`${API_URLS.GET_COURSE_TYPES}`);
   }
 
+  createCourseType(name: string): Observable<CourseType> {
+    return this.http.post<CourseType>(`${API_URLS.GET_COURSE_TYPES}`, { name });
+  }
+
+  updateCourseType(courseTypeId: number, name: string): Observable<CourseType> {
+    return this.http.put<CourseType>(`${API_URLS.GET_COURSE_TYPES}/${courseTypeId}`, { name });
+  }
+
+  deleteCourseType(courseTypeId: number): Observable<void> {
+    return this.http.delete<void>(`${API_URLS.GET_COURSE_TYPES}/${courseTypeId}`);
+  }
+
   getLevels(): Observable<Level[]> {
     return this.http.get<Level[]>(`${API_URLS.GET_LEVELS}`);
   }
+
+  createLevel(name: string): Observable<Level> {
+    return this.http.post<Level>(`${API_URLS.GET_LEVELS}`, { name });
+  }
+
+  updateLevel(levelId: number, name: string): Observable<Level> {
+    return this.http.put<Level>(`${API_URLS.GET_LEVELS}/${levelId}`, { name });
+  }
+
+  deleteLevel(levelId: number): Observable<void> {
+    return this.http.delete<void>(`${API_URLS.GET_LEVELS}/${levelId}`);
+  } 
 
 }
